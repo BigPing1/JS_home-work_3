c682c647d3fc7bd8de12a61b23c30e59f4024315
Danylo Deineha <deinehadanylo@gmail.com>
2024-05-24 16:18:38 +0100

    Im finished third home work

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Task 1
+// function checkAge(age) {
+//   age >= 18 ? console.log(true) : console.log("Чи дозволили батьки?");
+// }
+
+// checkAge(12);
+
+// Task 2
+
+// const comparsion = (a, b) => {
+//   a > b ? console.log(b) : console.log(a);
+// };
+
+// comparsion(14, 13);
+
+// Task 3
+
+// const ask = (question, yes, no) => {
+//   if (confirm(question)) yes();
+//   else no();
+// };
+
+// const showOk = () => {
+//   console.log("Your answer yes");
+// };
+// const showNo = () => {
+//   console.log("Your answer No");
+// };
+
+// ask("Ви згодні?", showOk, showNo);
